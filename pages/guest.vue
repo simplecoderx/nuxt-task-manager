9f56d510c580cb80a1913dcbcd6aa2478b24e60f
Lyn Vil <91258211+simplecoderx@users.noreply.github.com>
2024-02-24 15:20:33 +0800

    Initial Commit with Sign In Page and Protected Page together with Github Integration

---
+++ pages/guest.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>This is guest view.</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: '/private',
+  },
+})
+</script>
+
+<style>
+
+</style>
---
+++ pages/private.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1> This is private page. Only authenticated users can view this.</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({ middleware: 'auth' })
+</script>
+
+<style>
+
+</style>
